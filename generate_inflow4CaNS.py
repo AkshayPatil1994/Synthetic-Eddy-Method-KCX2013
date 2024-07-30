d58c22ea6b02904a20f2cd20217d868cda870909
Akshay Patil <patil.akshay11235@gmail.com>
2024-07-30 15:02:18 +0200

    update what z means

--- generate_inflow4CaNS.py
+++ generate_inflow4CaNS.py
@@ -2,16 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 # USER INPUT
-plotData = False
-saveData = True
+plotData = True
+saveData = False
 saveSep = False
+# FLOW SCALING PARAMETERS
+Re = 392.24
+H = 1.0
+kvisc = 1e-6
 # Name of the output files
-outfilename='inflow.dat'
+utau = Re*kvisc/H
+inflowOutFilename='inflow_new.dat'
 meanoutfile = 'mean_interpolated_re395.dat'
 covaroutfile = 'covar_interpolated_re395.dat'
 # First read the fine data
-covar_org = np.loadtxt('covar_re350.dat',skiprows=1)
-mean_org = np.loadtxt('mean_re350.dat',skiprows=1)
+covar_org = np.loadtxt('covar_re395.dat',skiprows=1)
+mean_org = np.loadtxt('mean_re395.dat',skiprows=1)
 # Load the new grid to interpolate fine data
 zgrid = np.loadtxt('grid.out')
 myz = zgrid[:,1]         # Set the right z location
@@ -20,10 +25,10 @@ mean = np.zeros((len(myz),np.shape(mean_org)[1]))
 covar = np.zeros((len(myz),np.shape(covar_org)[1]))
 # Interpolate all data to coarse grid
 for i in range(2,np.shape(mean_org)[1]):
-    mean[:,i] = np.interp(myz,mean_org[:,1],mean_org[:,i])
+    mean[:,i] = np.interp(myz,mean_org[:,1]*(1.0/(Re/H)),mean_org[:,i]*utau)
 
 for j in range(2,np.shape(covar_org)[1]):
-    covar[:,j] = np.interp(myz,mean_org[:,1],covar_org[:,j])
+    covar[:,j] = np.interp(myz,mean_org[:,1]*(1.0/(Re/H)),covar_org[:,j]*utau**2)
 # Setup the z axis
 mean[:,1] = myz
 covar[:,1] = myz
@@ -31,13 +36,13 @@ covar[:,1] = myz
 if(plotData):
     plt.figure(1)
     plt.title('Mean velocity check')
-    plt.plot(mean_org[:,2],mean_org[:,1],'kd',label='reference',markerfacecolor='None')
+    plt.plot(mean_org[:,2]*utau,mean_org[:,1]*(1.0/(Re/H)),'kd',label='reference',markerfacecolor='None')
     plt.plot(mean[:,2],mean[:,1],'r+',label='coarse',markerfacecolor='None')
     plt.legend()
     plt.figure(2)
     plt.title('Covariance check')
     for j in range(2,np.shape(covar_org)[1]):
-        plt.plot(covar_org[:,j],covar_org[:,1],'kd',label='reference',markerfacecolor='None')
+        plt.plot(covar_org[:,j]*utau**2,covar_org[:,1]*(1.0/(Re/H)),'kd',label='reference',markerfacecolor='None')
         plt.plot(covar[:,j],covar[:,1],'r+',label='coarse',markerfacecolor='None')
         if(j==2):
             plt.legend()
@@ -62,7 +67,7 @@ if(saveData):
     myheader = ['z', 'Umean', 'Ruu', 'Ruv', 'Ruw', 'Rvu', 'Rvv', 'Rvw', 'Rwu', 'Rwv', 'Rww']
     df = pd.DataFrame(dataout, columns=myheader)
     # Save dataframe as double precision csv with whitespace
-    df.to_csv(outfilename, index=False, header=True, sep=' ', float_format='%.15e')
+    df.to_csv(inflowOutFilename, index=False, header=True, sep=' ', float_format='%.15e')
 # Save separate files for generating initial conditions
 if(saveSep):
     mymeanheader = ['y','y+','Umean','dUmean/dy' ,'Wmean','dWmean/dy' ,'Pmean']
